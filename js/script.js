b28309631f161ecce866809f532bf376cc0c2bb3
dinesh <dineshk281800@gmail.com>
2023-09-20 11:26:21 +0530

    remove unwanted commands

--- js/script.js
+++ js/script.js
@@ -1,64 +1,45 @@
-let str = "{([])()}";
-// console.log(str.length);
-let len = str.length;
-let arr = [];
-let result;
-if (len % 2 === 0) {
-  for (let i = 0; i < len; i++) {
-    let Letter = str[i];
-    arr.push(Letter);
-  }
+const errorMsg = document.querySelector(".msg");
+const correctMsg = document.querySelector(".right");
+const wrongMsg = document.querySelector(".wrong");
+const resetBtn = document.querySelector(".btn-2");
+
+const checking = () => {
+  const str = document.querySelector(".value").value;
+  console.log(str);
+  if (!str) return;
+  let len = str.length;
   console.log(len);
-  console.log(len / 2);
-  for (let k = 0; k < len / 2; k++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      let char = arr[j + 1].charCodeAt(0) - arr[j].charCodeAt(0);
-      // console.log(char);
-      if (char === 1 || char === 2) {
-        arr.splice(j, 2);
-        // console.log(R);
-        console.log(arr);
+  let result;
+  if (len % 2 === 0) {
+    let arr = [];
+    for (let i = 0; i < len; i++) {
+      let Letter = str[i];
+      arr.push(Letter);
+    }
+
+    for (let k = 0; k < len / 2; k++) {
+      for (let j = 0; j < arr.length - 1; j++) {
+        let char = arr[j + 1].charCodeAt(0) - arr[j].charCodeAt(0);
+        if (char === 1 || char === 2) {
+          arr.splice(j, 2);
+        }
       }
     }
+    result = arr.length === 0 ? 1 : 0;
+  } else {
+    result = 0;
   }
-  result = arr.length === 0 ? 1 : 0;
-} else {
-  result = 0;
-}
 
-result ? console.log("valid") : console.log("invalid");
-// console.log(r);
+  if (result) {
+    correctMsg.style.display = "inline";
+    wrongMsg.style.display = "none";
+  } else {
+    correctMsg.style.display = "none";
+    wrongMsg.style.display = "inline";
+  }
+};
 
-// let checking = function (str) {
-//   let arr = [];
-//   let arr1 = [];
-//   for (let i = 0; i < str.length; i++) {
-//     let char = str[i];
-//     if (char === "(" || char === "[" || char === "{") {
-//       arr.push(char);
-//     } else if (char === ")" || char === "]" || char === "}") {
-//       if (arr.length === 0) {
-//         return 0;
-//       } else {
-//         let rem = arr[arr.length - 1];
-//         if (rem === "(" || rem === "[" || rem === "{") {
-//           arr.pop();
-//         } else {
-//           return 0;
-//         }
-//       }
-//     }
-//   }
-//   if (arr.length === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
-// let str = "[(])";
-// if (checking(str)) {
-//   console.log("Valid");
-// } else {
-//   console.log("Invalid");
-// }
+resetBtn.addEventListener("click", () => {
+  correctMsg.style.display = "none";
+  wrongMsg.style.display = "none";
+});
